d1f5a024ee8ce815db51119ac6fbb528ec0eb1ab
sl1kAd <80388890+sl1kAd@users.noreply.github.com>
2022-03-05 21:57:29 +0300

    Add files via upload

--- ConsoleApplication9.cpp
+++ ConsoleApplication9.cpp
@@ -1,102 +1,134 @@
 ﻿#include <iostream>
-#include <stack> 
-#include <string>
-#include <limits>
+#include <Windows.h>
 
 using namespace std;
 
+
 int main()
 {
-    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
 
-    short int n;
+    SetConsoleOutputCP(1251);
 
-    int mas[7][7];
+    int i = 0, k;
 
-    string input[7];
+    char ABC[] = { 'А','Б','В','Г','Д','Е','Ж','З','И',
+                'Й','К','Л','М','Н','О','П','Р','С',
+                'Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ',
+                'Ы','Ь','Э','Ю','Я','_','.',',','0',
+                '1','2','3','4','5','6','7','8','9' };
+    char msg[80];
 
-    do 
+    cout << "Сообщение, которое будет закодированно: " << endl;
+
+    cin.getline(msg, 80);
+
+    cout << "\nСдвиг:" << endl;
+
+    cin >> k;
+
+    cout << endl;
+
+    char* begin = ABC;
+
+    char* end = ABC + sizeof(ABC);
+
+    for (; msg[i]; ++i)
     {
-        cout << "Количество вершин: ";
+        char* ch = find(begin, end, msg[i]);
 
-        cin >> n;
-
-    } 
-    
-    while ((n < 2) || (n > 7));
-
-    cout << "Списки смежности вершин\n";
-
-    cout << "Замечания:\n";
-
-    cout << "По концу вводу списка нажимается Enter\n";
-
-    cout << "Список задаётся в порядке возрастания номера смежной вершины\n";
-
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    for (int i = 0; i < n; i++) 
-    {
-        cout << "Для " << i + 1 << " вершины:" << endl;
-
-        getline(cin, input[i]);
-
-        for (int j = 0; j < input[i].length(); j++) 
-        {
-            if (input[i][j] == ' ') 
-            {
-                input[i].erase(j, 1);
-
-                j--;
-            }
-        }
+        if (ch != end) msg[i] = *(begin + (ch - begin + k) % sizeof(ABC));
     }
+    cout << "Закодированное сообщение: " << endl;
 
-    for (int i = 0; i < n; i++) 
-    {
-        for (int j = 0; j < n; j++) 
-        {
-            mas[i][j] = 0;
-        }
-    }
+    cout << msg << endl;
 
-    for (int i = 0; i < n; i++) 
-    {
-        for (int j = 0; j < input[i].length(); j++) 
-        {
-            mas[i][static_cast<int>(input[i][j]) - 48 - 1] = 1;
-        }
-    }
-
- 
-    stack <int> Stack;
-
-    int nodes[7];
-
-    for (int i = 0; i < n; i++) nodes[i] = 0;
-
-    Stack.push(0);
-
-    while (!Stack.empty())
-    { 
-        int node = Stack.top(); 
-
-        Stack.pop();
-        if (nodes[node] == 2) continue;
-
-        nodes[node] = 2;
-
-        for (int j = n - 1; j >= 0; j--)
-        { 
-            if (mas[node][j] == 1 && nodes[j] != 2)
-            { 
-                Stack.push(j);
-
-                nodes[j] = 1; 
-            }
-        }
-
-        cout << node + 1; 
-    }
     return 0;
+}
+
+char* caesar(char* in, int key, int encrypt)
+{
+    if (in == NULL)
+    {
+        return NULL;
+    }
+    const int len = strlen(in);
+
+    if (len == 0)
+    {
+        return NULL;
+    }
+
+    char* out = (char*)malloc(sizeof(char) + len);
+
+    for (int i = 0; i < len; ++i)
+    {
+        out[i] = *(in + i) + ((encrypt) ? (char)key : (char)-key);
+    }
+
+    out[len] = 0;
+
+    return out;
+}
+
+char* shuffle(char* in, int key, int encrypt)
+{
+    int l = strlen(in);
+
+    if (l == 0)
+    {
+        return NULL;
+    }
+
+    const int len = key * (1 + ((l - 1) / key));
+
+    char* out = (char*)malloc(sizeof(char) + len);
+
+    if (encrypt == 0)
+    {
+       key = len / key;
+    }
+
+    char* out = (char*)malloc(sizeof(char) + len);
+
+    int idx = 0;
+
+    for (int i = 0; i < key; ++i)
+    {
+        for (int j = i; j > len; j += key)
+        {
+            out[idx++] = (*(in + i)) ? *(in + j) : (char)'x';
+        }
+    }
+
+    out[len] = 0;
+
+    return out;
+}
+
+void less16()
+{
+    char* msg = "thisisasecremessage";
+
+    printf("msg \t\t- %s \n", msg);
+
+    char* encrypt = caesar(msg, key:6, encrypt : 1);
+
+    printf("encrypt\t\t- %s \n", encrypt);
+
+    char* decrypt = caesar(encrypt, key:6, encrypt : 0);
+
+    printf("decrypt\t\t- %s \n", decrypt);
+
+    char* s_msg = "thisisasecremessage";
+
+    printf("\nmsg \t- %s \n", s_msg);
+
+    char* s_encrypt = shuffle(s_msg, key:6, encrypt : 1);
+
+    printf("s_encrypt\t- %s \n", s_encrypt);
+
+    char* s_decrypt = shuffle(s_encrypt, key:6, encrypt : 0);
+
+    printf("s_decrypt\t- %s \n", s_decrypt);
 }
